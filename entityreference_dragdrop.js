c34bf525213d558bf317fab7f4063f5bbdeed3eb
sergei_brill <brilsergei@gmail.com>
2014-07-03 16:21:18 +0400

    Update js and module files to make module working

--- entityreference_dragdrop.js
+++ entityreference_dragdrop.js
@@ -1,22 +1,40 @@
-jQuery(document).ready(function($) {
-  var $avail = $(".entityreference-dragdrop-available");
-  var $select = $(".entityreference-dragdrop-selected");
+(function($){
   
-  $avail.sortable({
-    connectWith: "ul.entityreference-dragdrop"
-  });
-
-  $select.sortable({
-    connectWith: "ul.entityreference-dragdrop",
-    update: entityreference_dragdrop_update
-  });
-});
-
-function entityreference_dragdrop_update(event, ui) {
-  var items = [];
-  jQuery(".entityreference-dragdrop-selected li").each(function(index) {
-    items.push(jQuery(this).attr('data-id'));
-  });
+  Drupal.entityreference_dragdrop = {};
   
-  jQuery("input.entityreference-dragdrop-values").val(items.join(','));
-}
+  Drupal.entityreference_dragdrop.update = function (event, ui) {
+    var items = [];
+    var ec = $(event.target).attr("data-ec");
+    $(".entityreference-dragdrop-selected[data-ec=" + ec + "] li").each(function(index) {
+      items.push($(this).attr('data-id'));
+    });
+    $("input.entityreference-dragdrop-values[data-ec=" + ec +"]").val(items.join(','));
+    
+    if (Drupal.settings.entityreference_dragdrop[ec] != -1) {
+      if (items.length > Drupal.settings.entityreference_dragdrop[ec]) {
+        $(".entityreference-dragdrop-message[data-ec=" + ec + "]").show();
+        $(".entityreference-dragdrop-selected[data-ec=" + ec + "]").css("border", "2px solid red");
+      }
+      else {
+        $(".entityreference-dragdrop-message[data-ec=" + ec + "]").hide();
+        $(".entityreference-dragdrop-selected[data-ec=" + ec + "]").css("border", "none");
+      }
+    }
+  }
+  
+  Drupal.behaviors.entityreference_dragdrop = {
+    attach: function() {
+      var $avail = $(".entityreference-dragdrop-available");
+      var $select = $(".entityreference-dragdrop-selected");
+
+      $avail.sortable({
+        connectWith: "ul.entityreference-dragdrop"
+      });
+
+      $select.sortable({
+        connectWith: "ul.entityreference-dragdrop",
+        update: Drupal.entityreference_dragdrop.update
+      });
+    }
+  };
+})(jQuery);
