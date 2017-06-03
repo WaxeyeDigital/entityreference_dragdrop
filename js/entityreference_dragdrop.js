017ddf6c0f65df4e2b15c1f7e8c9438f2881a66c
brilsergei <brilsergei@gmail.com>
2017-06-04 00:44:52 +0300

    Issue #2869379 by drc0: on first load of the page filter if input filled

--- js/entityreference_dragdrop.js
+++ js/entityreference_dragdrop.js
@@ -1,7 +1,8 @@
 (function($){
   
   Drupal.entityreference_dragdrop = Drupal.entityreference_dragdrop ? Drupal.entityreference_dragdrop : {};
-  
+
+  // Used a 'update' event handler for sortable plugin.
   Drupal.entityreference_dragdrop.update = function (event, ui) {
     var items = [];
     var key = $(event.target).attr("data-key");
@@ -19,6 +20,26 @@
         $(".entityreference-dragdrop-message[data-key=" + key + "]").hide();
         $(".entityreference-dragdrop-selected[data-key=" + key + "]").css("border", "");
       }
+    }
+  };
+
+  // Filters items in a widget.
+  Drupal.entityreference_dragdrop.filter = function (input) {
+    var $input = $(input);
+    var val = $input.val().toLowerCase();
+    if (val != '') {
+      $input.parents('.entityreference-dragdrop-container').find('li.ui-sortable-handle').each(function(i, elem) {
+        var $elem = $(elem);
+        if ($elem.data('label').toLowerCase().indexOf(val) >= 0) {
+          $elem.show();
+        }
+        else {
+          $elem.hide();
+        }
+      });
+    }
+    else {
+      $input.parents('.entityreference-dragdrop-container').find('li').show();
     }
   };
   
@@ -44,23 +65,9 @@
       });
 
       $('.entityreference-dragdrop-filter').once('entityreference-dragdrop').each(function() {
+        Drupal.entityreference_dragdrop.filter(this);
         $(this).bind('keyup paste', function() {
-          var $this = $(this);
-          var val = $this.val().toLowerCase();
-          if (val != '') {
-            $this.parents('.entityreference-dragdrop-container').find('li.ui-sortable-handle').each(function(i, elem) {
-              var $elem = $(elem);
-              if ($elem.data('label').toLowerCase().indexOf(val) >= 0) {
-                $elem.show();
-              }
-              else {
-                $elem.hide();
-              }
-            });
-          }
-          else {
-            $this.parents('.entityreference-dragdrop-container').find('li').show();
-          }
+          Drupal.entityreference_dragdrop.filter(this);
         });
       });
     }
